1e40829974e7b645d265c37eb8672b27ff8e9701
Ales Vlchytski <vilchytski.ales@yandex.by>
2014-08-21 23:19:39 +0300

    Deleted obsolete code

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -1,3 +1,3 @@
 // require of ace moved to implicit javascript_include_tag's due to javascript-worker.js
 // load failures (when ace loads it dynamically and sends request to wrong URL)
-'';
+// require tree ./editor

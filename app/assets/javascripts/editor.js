03f19c9cf9215525940dd13e8d5041c34e786fa3
Ales Vilchytski <vilchytski.ales@yandex.by>
2014-10-10 23:09:39 +0300

    Issue with assets (YUI-compressor) fixed

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -1,3 +1,4 @@
 // require of ace moved to implicit javascript_include_tag's due to javascript-worker.js
 // load failures (when ace loads it dynamically and sends request to wrong URL)
 // require tree ./editor
+''; //hack for YUI compressor issue, don't delete this line. https://github.com/yui/yuicompressor/issues/130

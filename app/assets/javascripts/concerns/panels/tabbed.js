428dfb259be2826fcc5de08ab96f0dcec1c33afa
Ales Vilchytski <vilchytski.ales@yandex.by>
2014-11-30 23:04:06 +0300

    Tabbed panel redesigned

--- app/assets/javascripts/concerns/panels/tabbed.js
+++ app/assets/javascripts/concerns/panels/tabbed.js
@@ -1,14 +1,28 @@
 source('concerns/panelTabbed', ['concerns/concerns', 'concerns/panelBase'], function() {
     concerns.inheritWidgetBase('panelTabbed', $.concerns.panelBase, {
+        _create: function() {
+            this._super();
+            var id = this.getId();
+            var $header = this._getHeader$();
+            var $tab_input = $('input[data-type="tab-input"]', $header);
+            
+            $('a[data-toggle="tab"]').on('shown.bs.tab', function() {
+                var $atab = $(this);
+                $tab_input.val($atab.data('value'));
+            });
         
-        /** TODO add functions:
-         * - toggleTab(i)
-         * - currentTab() -> int
-         * 
-         * and override:
-         * - setHeader(text)
-         * - setBody(text)
-         */
-        
+            /** TODO add functions:
+             * - toggleTab(i)
+             * - currentTab() -> int
+             * 
+             * and override:
+             * - setHeader(text)
+             * - setBody(text)
+             */
+            
+            this.setHeader = function() { throw 'not implemented'; };
+            this.setBody = function() { throw 'not implemented'; };
+        },
+         
     });
 });
